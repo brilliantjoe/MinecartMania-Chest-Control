4ef8dbdead02b8920930533d3e6ee4ae37d648f8
Afforess <afforess@gmail.com>
2011-02-15 23:41:08 -0500

    Configuration change.
    Signed-off-by: Afforess <afforess@gmail.com>

--- src/com/afforess/minecartmaniachestcontrol/SettingList.java
+++ src/com/afforess/minecartmaniachestcontrol/SettingList.java
@@ -9,6 +9,12 @@ public class SettingList {
 				Boolean.TRUE, 
 				"Storage Carts will automatically grab and store any nearby items on the ground, if they have room",
 				MinecartManiaChestControl.description.getName()
+		),
+		new Setting(
+				"Nearby Items Range", 
+				new Integer(2), 
+				"The range to search for items to pick up. Warning: very large values may cause items to dissappear from half a map away, confusing players.",
+				MinecartManiaChestControl.description.getName()
 		)
 	};
 }
