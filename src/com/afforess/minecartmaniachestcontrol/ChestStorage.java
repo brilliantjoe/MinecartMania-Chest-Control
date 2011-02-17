7e7b37adc758fbdf0c06c569e18dceeb77a8594f
Afforess <afforess@gmail.com>
2011-02-16 21:55:16 -0500

    Chest collecting and chest depositing now dynamically supported
    Signed-off-by: Afforess <afforess@gmail.com>

--- src/com/afforess/minecartmaniachestcontrol/ChestStorage.java
+++ src/com/afforess/minecartmaniachestcontrol/ChestStorage.java
@@ -6,65 +6,73 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.block.Sign;
-import org.bukkit.inventory.Inventory;
 
 import com.afforess.minecartmaniacore.MinecartManiaChest;
+import com.afforess.minecartmaniacore.MinecartManiaInventory;
 import com.afforess.minecartmaniacore.MinecartManiaMinecart;
 import com.afforess.minecartmaniacore.MinecartManiaStorageCart;
 import com.afforess.minecartmaniacore.MinecartManiaWorld;
 import com.afforess.minecartmaniacore.utils.SignUtils;
 import com.afforess.minecartmaniacore.utils.ItemUtils;
+import com.afforess.minecartmaniacore.utils.StringUtils;
 
 public abstract class ChestStorage {
 
 	public static boolean doChestStorage(MinecartManiaStorageCart minecart) {
 		ArrayList<Block> blockList = minecart.getParallelBlocks();
-		Inventory inv = minecart.getInventory();
 		boolean action = false;
 		
 		for (Block block : blockList) {
 			if (block.getState() instanceof Chest) {
 				MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest)block.getState());
 				ArrayList<Sign> signList = SignUtils.getAdjacentSignList(chest.getWorld(), chest.getX(), chest.getY(), chest.getZ(), 1);
+				
 				for (Sign sign : signList) {
+					MinecartManiaInventory withdraw = null;
+					MinecartManiaInventory deposit = null;
+					
 					if (sign.getLine(0).toLowerCase().contains("collect items")) {
 						sign.setLine(0, "[Collect Items]");
-						for (int i = 1; i < 4; i++) {
-							if (sign.getLine(i).trim().isEmpty()) {
-								continue;
-							}
-							
-							if (sign.getLine(i).toLowerCase().contains("all items")) {
-								sign.setLine(i, "[All Items]");
-								//Transfer as much as possible to the other chest
-								for (int j = 0; j < inv.getSize(); j++) {
-									if (!chest.addItem(inv.getItem(j))) {
-										break;
-									}
-									inv.clear(j);
+						withdraw = (MinecartManiaInventory)minecart;
+						deposit = (MinecartManiaInventory)chest;
+					}
+					if (sign.getLine(0).toLowerCase().contains("deposit items")) {
+						sign.setLine(0, "[Deposit Items]");
+						withdraw = (MinecartManiaInventory)chest;
+						deposit = (MinecartManiaInventory)minecart;
+					}
+					for (int i = 1; i < 4; i++) {
+						if (sign.getLine(i).trim().isEmpty()) {
+							continue;
+						}
+						if (sign.getLine(i).toLowerCase().contains("all items")) {
+							sign.setLine(i, "[All Items]");
+							//Transfer as much as possible
+							for (int j = 0; j < withdraw.size(); j++) {
+								if (!deposit.addItem(withdraw.getItem(j))) {
+									break;
 								}
+								deposit.setItem(j, null);
 							}
-							
+						}
+						else {
 							Material[] items = ItemUtils.getItemStringToMaterial(sign.getLine(i).toLowerCase());
 							for (Material m : items) {
 								if (m != null) {
-									if (!sign.getLine(i).contains("[")) {
-										sign.setLine(i, "["+sign.getLine(i)+"]");
-									}
-									while (inv.contains(m)) {
+									sign.setLine(i, StringUtils.addBrackets(StringUtils.removeBrackets(sign.getLine(i))));
+									while (withdraw.contains(m)) {
 										action = true;
-										if (!chest.addItem(inv.getItem(inv.first(m)))) {
+										if (!deposit.addItem(withdraw.getItem(withdraw.getInventory().first(m)))) {
 											break;
 										}
-										inv.clear(inv.first(m));
+										withdraw.setItem(withdraw.getInventory().first(m), null);
 									}
 								}
 							}
 						}
-						sign.update();
 					}
+					sign.update();
 				}
-				chest.update();
 			}
 		}
 		return action;
